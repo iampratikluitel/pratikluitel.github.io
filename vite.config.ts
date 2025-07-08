e818a35f53b3ce5bbc9f91490ec18f9ee70a7d4e
Pratik Luitel <84219777+iampratikluitel@users.noreply.github.com>
2025-07-08 17:36:00 +0545

    final changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/' : '/',
+  build: {
+    outDir: 'docs',
+  },
   server: {
     host: "::",      // Listen on all interfaces
     port: 8080,      // Development server port
